0ff62c48e599fa2070ed66f9df889c05ecfe23f3
Vaishnave Subbramanian <vaishnave.subbramanian@psiog.com>
2020-08-10 13:54:01 +0530

    Added OWIN/Corrs packages

--- BMSWebApp1/App_Start/WebApiConfig.cs
+++ BMSWebApp1/App_Start/WebApiConfig.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
 
 namespace BMSWebApp1
 {
@@ -10,15 +16,29 @@ namespace BMSWebApp1
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
+            // Web API configuration and services
+            config.Formatters.JsonFormatter.AddUriPathExtensionMapping("json", "application/json");
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            json.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            json.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new { id = RouteParameter.Optional });
         }
     }
 }
--- BMSWebApp1/Controllers/UserController.cs
+++ BMSWebApp1/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using BMSDbEntities;
 using BMSWebApp1.Helper;
@@ -12,11 +13,121 @@ namespace BMSWebApp1.Controllers
     public class UserController : ApiController
     {
         [HttpPost]
-        [Route("api/user/registeruser")]
-        public void PostRegisterUser([FromBody] CUSTOMER customer)
+        [Route("api/user/user")]
+        public IHttpActionResult PostRegisterUser([FromBody] CUSTOMER customer)
         {
-            BMSDbMethods.UserRegistration(customer);
+            try
+            {
+                var returnStatus = BMSDbMethods.UserRegistration(customer);
+                if (returnStatus>0)
+                {
+                    return Ok();
+
+                }
+                else
+                {
+                    throw new InvalidOperationException("User registration failed.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Log.Write(ex);
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Database connection problem")); ;
+            }
+        }
+        [HttpGet]
+        [Route("api/user/users")]
+
+        public IHttpActionResult GetUsers()
+        {
+            try
+            {
+                using (BMSApplicationEntities entities = new BMSApplicationEntities())
+                {
+                    return Ok(entities.CUSTOMERs.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Log.Write(ex);
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Database connection problem")); ;
+            }
         }
 
+        [HttpGet]
+        [Route("api/user/user/{id?}")]
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                using (BMSApplicationEntities entities = new BMSApplicationEntities())
+                {
+                    return Ok(entities.CUSTOMERs.FirstOrDefault(c => c.CustomerID == id));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Log.Write(ex);
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Database connection problem")); ;
+            }
+        }
+
+
+        // PUT api/values/5
+        [HttpPut]
+
+        [Route("api/user/user/{id?}")]
+        public IHttpActionResult Put(int id, [FromBody] CUSTOMER customer)
+        {
+            try
+            {
+                using (BMSApplicationEntities entities = new BMSApplicationEntities())
+                {
+                    var entity = entities.CUSTOMERs.FirstOrDefault(c => c.CustomerID == id);
+                    entity.CustomerName = customer.CustomerName;
+                    entity.CustomerEmail = customer.CustomerEmail;
+                    entity.CustomerPassword = customer.CustomerEmail;
+                    entity.CustomerAddress = customer.CustomerAddress;
+                    entity.CustomerContact = customer.CustomerContact;
+                    entities.SaveChanges();
+                    return Ok();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Log.Write(ex);
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Database connection problem")); ;
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/user/user/{id?}")]
+        public IHttpActionResult Delete(int id)
+        {
+
+        try
+        {
+            using (BMSApplicationEntities entities = new BMSApplicationEntities())
+            {
+                entities.CUSTOMERs.Remove(entities.CUSTOMERs.FirstOrDefault(c => c.CustomerID == id));
+                entities.SaveChanges();
+                return Ok();
+
+            }
+        }
+        catch (Exception ex)
+        {
+
+            Log.Write(ex);
+            return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Database connection problem")); ;
+        }
     }
+ }
+
 }
